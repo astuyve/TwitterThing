31bf884eb60397fb734b16af37aa783fbf5bec26
aj <astuyve@gmail.com>
2014-07-17 18:20:16 -0500

    Recompiling assets for heroku, moving maps.js back into root.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   def self.from_omniauth(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
@@ -7,7 +8,11 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["info"]["nickname"]
+      auth["info"].each do |attr, val|
+        if user.has_attribute?("#{attr}")
+          user.update_attribute("#{attr}", val)
+        end
+      end
     end
   end
 end
---
+++ db/migrate/20140717220959_add_info_hash_to_users.rb
@@ -0,0 +1,7 @@
+class AddInfoHashToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :nickname, :string
+    add_column :users, :location, :string
+    add_column :users, :description, :string
+  end
+end
