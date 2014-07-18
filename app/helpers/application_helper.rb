55db0fc6ea4bc3b643e27fe496ce3248f00d2311
aj <astuyve@gmail.com>
2014-07-18 07:22:15 -0500

    Bit more dynamic class methods for roles. Responds to user.role or user.admin? etc...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
+  def current_rule
+    @current_user.role
+  end
    $client = Twitter::REST::Client.new do |config|
     config.consumer_key = Figaro.env.twitter_api_key
     config.consumer_secret = Figaro.env.twitter_api_secret
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,18 @@ class User < ActiveRecord::Base
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
 
-
+  ROLES=["user", "manager", "admin"]
+  ROLES.each do |user_role|
+    define_method "#{user_role}?" do
+      self.role == user_role
+    end
+  end
   def self.create_from_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
+      # Grab a random role to demo with
+      user.role = ROLES.sample
       auth["info"].each do |attr, val|
         if user.has_attribute?("#{attr}")
           user.update_attribute("#{attr}", val)
